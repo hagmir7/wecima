902049eb84c629a778db3347bd233e95b00f81d5
Hassan Agmir <hagmir6@gmail.com>
2022-11-20 19:12:01 +0100

    first commit

--- agmir/settings.py
+++ agmir/settings.py
@@ -19,7 +19,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# DEBUG_PROPAGATE_EXCEPTIONS = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
