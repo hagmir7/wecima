ca0d549da52477cb8bf989cd6911203f23f49afc
Hassan Agmir <hagmir6@gmail.com>
2023-11-20 12:40:52 +0000

    Media files configurations

--- agmir/urls.py
+++ agmir/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -7,21 +6,21 @@ from scraping.sitemap import PostsSitemap, StaticViewSitemap
 from django.contrib.sitemaps.views import sitemap
 
 sitemaps = {
-    'static': StaticViewSitemap,
-    'posts': PostsSitemap,
+    "static": StaticViewSitemap,
+    "posts": PostsSitemap,
 }
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('scraping.urls')),
-    path('', include('users.urls')),
-    path('api/', include('rest_framework.urls')),
-    path('summernote/', include('django_summernote.urls')),
-    path('robots.txt', include('robots.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path("admin/", admin.site.urls),
+    path("", include("scraping.urls")),
+    path("", include("users.urls")),
+    path("api/", include("rest_framework.urls")),
+    path("summernote/", include("django_summernote.urls")),
+    path("robots.txt", include("robots.urls")),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
 ]
 
-urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
