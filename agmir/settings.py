956a2a8349db87f5d60fdc1fb1c69fb20b5323a3
Hassan Agmir <hagmir6@gmail.com>
2023-11-20 12:24:07 +0000

    Update Secret key

--- agmir/settings.py
+++ agmir/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY =  "kbt*xsb#fk!n4!ae_nui2+nu-9siz^cta9wgy6mgwf-e(*vk1x" #os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
